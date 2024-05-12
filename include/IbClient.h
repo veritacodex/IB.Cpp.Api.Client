930ec1f50a177d729788303290e3b3e638fe3a5c
AnAlgoTrader <verita.codex@gmail.com>
2024-05-12 23:50:55 +0200

    .

--- include/IbClient.h
+++ include/IbClient.h
@@ -1,8 +1,6 @@
 #ifndef IBCLIENT_H
 #define IBCLIENT_H
 
-#include <iostream>
-
 namespace IbApiClient {
     class IbClient {
         public:
--- src/IbClientPositions.cpp
+++ src/IbClientPositions.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "IbClient.h"
 
 void IbApiClient::IbClient::methodTwo() {
