440a6f05610584e012f9321122b535e59069f461
AnAlgoTrader <verita.codex@gmail.com>
2024-05-14 01:45:27 +0200

    .

--- library/inc/IbClient.h
+++ library/inc/IbClient.h
@@ -1,6 +1,6 @@
 #ifndef IBCLIENT_H
 #define IBCLIENT_H
-#include "../../Third-Party/tws-api/EWrapper.h"
+#include "../../third-party/tws-api/EWrapper.h"
 
 namespace IbApiClient {
     class IbClient : public EWrapper {
