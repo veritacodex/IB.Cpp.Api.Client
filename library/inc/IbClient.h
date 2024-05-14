7f0edd1a4fb988e425ed9e670f06e346fb6cac4f
AnAlgoTrader <verita.codex@gmail.com>
2024-05-14 11:05:26 +0200

    .

--- library/inc/IbClient.h
+++ library/inc/IbClient.h
@@ -8,27 +8,24 @@
 
 #include <ConnectionState.h>
 
-namespace IbApiClient
-{
-    class IbClient : public EWrapper
-    {
-    public:
-        IbClient();
-        ~IbClient() override;
+namespace IbApiClient {
+    class IbClient : public EWrapper {
+        public:
+            IbClient();
+            ~IbClient() override;
 
-        void connect(const char *host, const int &port, const int &clientId);
-        void disconnect() const;
-        bool isConnected() const;
-
-    private:
-        EReaderOSSignal m_osSignal{2000};
-        EClientSocket *const m_pClient;
-        ConnectionState m_state{ConnectionState::ST_CONNECT};
-        time_t m_sleepDeadline{0};
-        OrderId m_orderId{0};
-        std::unique_ptr<EReader> m_pReader;
-        bool m_extraAuth{false};
-        std::string m_bboExchange;
+            void connect(const char *host, const int &port, const int &clientId);
+            void disconnect() const;
+            bool isConnected() const;
+        private:
+            EReaderOSSignal m_osSignal{2000};
+            EClientSocket *const m_pClient;
+            ConnectionState m_state{ConnectionState::ST_CONNECT};
+            time_t m_sleepDeadline{0};
+            OrderId m_orderId{0};
+            std::unique_ptr<EReader> m_pReader;
+            bool m_extraAuth{false};
+            std::string m_bboExchange;
     };
 }
 #endif
--- library/src/IbClient.cpp
+++ library/src/IbClient.cpp
@@ -2,30 +2,26 @@
 #include <iostream>
 #include <memory>
 
-IbApiClient::IbClient::IbClient() : m_pClient(new EClientSocket(this, &m_osSignal)) {}
+IbApiClient::IbClient::IbClient() : m_pClient(new EClientSocket(this, &m_osSignal)) {
+}
 
 IbApiClient::IbClient::~IbClient() { disconnect(); }
 
-void IbApiClient::IbClient::connect(const char *host, const int &port, const int &clientId)
-{
+void IbApiClient::IbClient::connect(const char *host, const int &port, const int &clientId) {
     std::cout << "Connecting to host: " << host << " on port:" << port << " with clientId:" << clientId << std::endl;
 
-    if (m_pClient->eConnect(host, port, clientId, m_extraAuth))
-    {
+    if (m_pClient->eConnect(host, port, clientId, m_extraAuth)) {
         std::cout << "Connected" << std::endl;
         m_pReader = std::make_unique<EReader>(m_pClient, &m_osSignal);
         m_pReader->start();
-    }
-    else
-        std::cout << "Cannot connect";
+    } else
+        std::cout << "Cannot connect" << std::endl;
 }
 
-void IbApiClient::IbClient::disconnect() const
-{
+void IbApiClient::IbClient::disconnect() const {
     m_pClient->eDisconnect();
 }
 
-bool IbApiClient::IbClient::isConnected() const
-{
+bool IbApiClient::IbClient::isConnected() const {
     return m_pClient->isConnected();
 }
