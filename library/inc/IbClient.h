343e88d6053d73e2a32e1fc8b64b82da63fd16dd
AnAlgoTrader <verita.codex@gmail.com>
2024-05-14 09:01:50 +0200

    .

--- library/inc/IbClient.h
+++ library/inc/IbClient.h
@@ -1,9 +1,32 @@
-#ifndef IBCLIENT_H
-#define IBCLIENT_H
-#include "../../third-party/tws-api/EWrapper.h"
+#ifndef IBAPICLIENT_IBCLIENT_H
+#define IBAPICLIENT_IBCLIENT_H
 
-namespace IbApiClient {
-    class IbClient : public EWrapper {
+#include "../../third-party/tws-api/EWrapper.h"
+#include "../../third-party/tws-api/EReaderOSSignal.h"
+#include "../../third-party/tws-api/EClientSocket.h"
+#include "../../third-party/tws-api/EReader.h"
+
+#include <ConnectionState.h>
+
+namespace IbApiClient
+{
+    class IbClient : public EWrapper
+    {
+    public:
+        IbClient();
+        ~IbClient();
+
+    private:
+        EReaderOSSignal m_osSignal;
+        EClientSocket *const m_pClient;
+
+        ConnectionState m_state;
+        time_t m_sleepDeadline;
+
+        OrderId m_orderId;
+        std::unique_ptr<EReader> m_pReader;
+        bool m_extraAuth;
+        std::string m_bboExchange;
     };
 }
-#endif // IBCLIENT_H
+#endif
