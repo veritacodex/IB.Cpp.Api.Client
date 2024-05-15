d997d6712b5a10d4cd24c8c0a88a4cdcf76c913f
AnAlgoTrader <verita.codex@gmail.com>
2024-05-15 09:19:30 +0200

    .

--- library/inc/model/Account.h
+++ library/inc/model/Account.h
@@ -7,11 +7,54 @@ namespace IbApiClient {
     struct Account {
         std::string id;
         std::string accountType;
+        double cushion{};
+        double lookAheadNextChange{};
+        double accruedCash{};
+        double availableFunds{};
+        double buyingPower{};
+        double equityWithLoanValue{};
+        double excessLiquidity{};
+        double fullAvailableFunds{};
+        double fullExcessLiquidity{};
+        double fullInitMarginReq{};
+        double fullMaintMarginReq{};
+        double grossPositionValue{};
+        double initMarginReq{};
+        double lookAheadAvailableFunds{};
+        double lookAheadExcessLiquidity{};
+        double lookAheadInitMarginReq{};
+        double lookAheadMaintMarginReq{};
+        double maintMarginReq{};
+        double netLiquidation{};
+        double totalCashValue{};
+
+        Account() = default;
         explicit Account(std::string id): id(std::move(id)) {
         }
         [[nodiscard]] std::string toString() const {
             std::string output = "Account summary (" + id + ")\n";
             output += "\tAccountType:" + accountType + "\n";
+            output += "\tCushion:" + std::to_string(cushion) + "\n";
+            output += "\tLookAheadNextChange:" + std::to_string(lookAheadNextChange) + "\n";
+            output += "\tAccruedCash:" + std::to_string(accruedCash) + "\n";
+            output += "\tAvailableFunds:" + std::to_string(availableFunds) + "\n";
+            output += "\tBuyingPower:" + std::to_string(buyingPower) + "\n";
+            output += "\tEquityWithLoanValue:" + std::to_string(equityWithLoanValue) + "\n";
+            output += "\tExcessLiquidity:" + std::to_string(excessLiquidity) + "\n";
+            output += "\tFullAvailableFunds:" + std::to_string(fullAvailableFunds) + "\n";
+            output += "\tFullExcessLiquidity:" + std::to_string(fullExcessLiquidity) + "\n";
+            output += "\tFullInitMarginReq:" + std::to_string(fullInitMarginReq) + "\n";
+            output += "\tFullMaintMarginReq:" + std::to_string(fullMaintMarginReq) + "\n";
+            output += "\tGrossPositionValue:" + std::to_string(grossPositionValue) + "\n";
+            output += "\tInitMarginReq:" + std::to_string(initMarginReq) + "\n";
+            output += "\tLookAheadAvailableFunds:" + std::to_string(lookAheadAvailableFunds) + "\n";
+            output += "\tLookAheadExcessLiquidity:" + std::to_string(lookAheadExcessLiquidity) + "\n";
+            output += "\tLookAheadInitMarginReq:" + std::to_string(lookAheadInitMarginReq) + "\n";
+            output += "\tLookAheadMaintMarginReq:" + std::to_string(lookAheadMaintMarginReq) + "\n";
+            output += "\tMaintMarginReq:" + std::to_string(maintMarginReq) + "\n";
+            output += "\tNetLiquidation:" + std::to_string(netLiquidation) + "\n";
+            output += "\tTotalCashValue:" + std::to_string(totalCashValue) + "\n";
+
             return output;
         }
     };
--- library/src/IbClientAccount.cpp
+++ library/src/IbClientAccount.cpp
@@ -1,6 +1,5 @@
 #include <IbClient.h>
 #include <ranges>
-
 #include "model/AccountSummaryTags.h"
 #include "utils/StringUtils.h"
 
@@ -11,15 +10,52 @@ void IbApiClient::IbClient::managedAccounts(const std::string &accountsList) {
         Accounts.insert({id, Account(id)});
     }
 }
-
 void IbApiClient::IbClient::accountSummary(int reqId, const std::string &account, const std::string &tag,
                                            const std::string &value, const std::string &currency) {
-    // if (tag == AccountSummaryTags::AccountType)
-    //     Accounts[account].accountType = value;
-    // else {
-    //     const std::string output = "Account:" + account + " tag:" + tag + " value:" + value;
-    //     spdlog::get("console")->info(output);
-    // }
+    if (tag == AccountSummaryTags::AccountType)
+        Accounts[account].accountType = value;
+    else if (tag == AccountSummaryTags::Cushion)
+        Accounts[account].cushion = stod(value);
+    else if (tag == AccountSummaryTags::LookAheadNextChange)
+        Accounts[account].lookAheadNextChange = stod(value);
+    else if (tag == AccountSummaryTags::AccruedCash)
+        Accounts[account].accruedCash = stod(value);
+    else if (tag == AccountSummaryTags::AvailableFunds)
+        Accounts[account].availableFunds = stod(value);
+    else if (tag == AccountSummaryTags::BuyingPower)
+        Accounts[account].buyingPower = stod(value);
+    else if (tag == AccountSummaryTags::EquityWithLoanValue)
+        Accounts[account].equityWithLoanValue = stod(value);
+    else if (tag == AccountSummaryTags::ExcessLiquidity)
+        Accounts[account].excessLiquidity = stod(value);
+    else if (tag == AccountSummaryTags::FullAvailableFunds)
+        Accounts[account].fullAvailableFunds = stod(value);
+    else if (tag == AccountSummaryTags::FullExcessLiquidity)
+        Accounts[account].fullExcessLiquidity = stod(value);
+    else if (tag == AccountSummaryTags::FullInitMarginReq)
+        Accounts[account].fullInitMarginReq = stod(value);
+    else if (tag == AccountSummaryTags::FullMaintMarginReq)
+        Accounts[account].fullMaintMarginReq = stod(value);
+    else if (tag == AccountSummaryTags::GrossPositionValue)
+        Accounts[account].grossPositionValue = stod(value);
+    else if (tag == AccountSummaryTags::InitMarginReq)
+        Accounts[account].initMarginReq = stod(value);
+    else if (tag == AccountSummaryTags::LookAheadAvailableFunds)
+        Accounts[account].lookAheadAvailableFunds = stod(value);
+    else if (tag == AccountSummaryTags::LookAheadExcessLiquidity)
+        Accounts[account].lookAheadExcessLiquidity = stod(value);
+    else if (tag == AccountSummaryTags::LookAheadInitMarginReq)
+        Accounts[account].lookAheadInitMarginReq = stod(value);
+    else if (tag == AccountSummaryTags::LookAheadMaintMarginReq)
+        Accounts[account].lookAheadMaintMarginReq = stod(value);
+    else if (tag == AccountSummaryTags::MaintMarginReq)
+        Accounts[account].maintMarginReq = stod(value);
+    else if (tag == AccountSummaryTags::NetLiquidation)
+        Accounts[account].netLiquidation = stod(value);
+    else if (tag == AccountSummaryTags::TotalCashValue)
+        Accounts[account].totalCashValue = stod(value);
+    else {
+    }
 }
 void IbApiClient::IbClient::accountSummaryEnd(int reqId) {
     for (const Account &account: Accounts | std::ranges::views::values) {
