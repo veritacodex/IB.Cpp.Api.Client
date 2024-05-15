b9a9a2a38b7c698b85b7b9db2690adf09cce110e
AnAlgoTrader <verita.codex@gmail.com>
2024-05-15 10:20:43 +0200

    .

--- library/src/IbClientListener.cpp
+++ library/src/IbClientListener.cpp
@@ -1,19 +1,18 @@
 #include <IbClient.h>
-#include <iostream>
 
 void IbApiClient::IbClient::listen() {
-    const time_t now = time(nullptr);
-    //std::cout << static_cast<int>(m_state) << std::endl;
+    auto const now = std::chrono::system_clock::now();
+    const time_t now_t = std::chrono::system_clock::to_time_t(now);
     switch (m_state) {
         using enum ConnectionState;
         case ST_PING_ACK:
-            if( m_sleepDeadline < now) {
+            if( m_sleepDeadline < now_t) {
                 disconnect();
                 return;
             }
         break;
         case ST_IDLE:
-            if( m_sleepDeadline < now) {
+            if( m_sleepDeadline < now_t) {
                 m_state = ST_PING;
                 return;
             }
