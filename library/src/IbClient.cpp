f8fe0096e2823386489d9b5574cb387020d2c5cf
AnAlgoTrader <verita.codex@gmail.com>
2024-05-15 10:08:39 +0200

    .

--- library/src/IbClient.cpp
+++ library/src/IbClient.cpp
@@ -8,7 +8,6 @@ IbApiClient::IbClient::~IbClient() {
     disconnect();
     if (m_pReader)
         m_pReader.reset();
-    delete m_pClient;
 }
 void IbApiClient::IbClient::connect(const char *host, const int &port, const int &clientId) {
     const std::string message = fmt::format("Connecting to host:{} on port:{} with clientId:{}",
