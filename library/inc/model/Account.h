a417d34ba22e40a69d5afa2cc34422a495e65c0c
AnAlgoTrader <verita.codex@gmail.com>
2024-05-15 09:49:05 +0200

    .

--- library/inc/model/Account.h
+++ library/inc/model/Account.h
@@ -33,27 +33,27 @@ namespace IbApiClient {
         }
         [[nodiscard]] std::string toString() const {
             std::string output = "Account summary (" + id + ")\n";
-            output += "\tAccountType:" + accountType + "\n";
-            output += "\tCushion:" + std::to_string(cushion) + "\n";
-            output += "\tLookAheadNextChange:" + std::to_string(lookAheadNextChange) + "\n";
-            output += "\tAccruedCash:" + std::to_string(accruedCash) + "\n";
-            output += "\tAvailableFunds:" + std::to_string(availableFunds) + "\n";
-            output += "\tBuyingPower:" + std::to_string(buyingPower) + "\n";
-            output += "\tEquityWithLoanValue:" + std::to_string(equityWithLoanValue) + "\n";
-            output += "\tExcessLiquidity:" + std::to_string(excessLiquidity) + "\n";
-            output += "\tFullAvailableFunds:" + std::to_string(fullAvailableFunds) + "\n";
-            output += "\tFullExcessLiquidity:" + std::to_string(fullExcessLiquidity) + "\n";
-            output += "\tFullInitMarginReq:" + std::to_string(fullInitMarginReq) + "\n";
-            output += "\tFullMaintMarginReq:" + std::to_string(fullMaintMarginReq) + "\n";
-            output += "\tGrossPositionValue:" + std::to_string(grossPositionValue) + "\n";
-            output += "\tInitMarginReq:" + std::to_string(initMarginReq) + "\n";
-            output += "\tLookAheadAvailableFunds:" + std::to_string(lookAheadAvailableFunds) + "\n";
-            output += "\tLookAheadExcessLiquidity:" + std::to_string(lookAheadExcessLiquidity) + "\n";
-            output += "\tLookAheadInitMarginReq:" + std::to_string(lookAheadInitMarginReq) + "\n";
-            output += "\tLookAheadMaintMarginReq:" + std::to_string(lookAheadMaintMarginReq) + "\n";
-            output += "\tMaintMarginReq:" + std::to_string(maintMarginReq) + "\n";
-            output += "\tNetLiquidation:" + std::to_string(netLiquidation) + "\n";
-            output += "\tTotalCashValue:" + std::to_string(totalCashValue) + "\n";
+            output += fmt::format("\tAccountType:{}\n", accountType);
+            output += fmt::format("\tCushion:{}\n", std::to_string(cushion));
+            output += fmt::format("\tLookAheadNextChange:{}\n", std::to_string(lookAheadNextChange));
+            output += fmt::format("\tAccruedCash:{}\n", std::to_string(accruedCash));
+            output += fmt::format("\tAvailableFunds:{}\n", std::to_string(availableFunds));
+            output += fmt::format("\tBuyingPower:{}\n", std::to_string(buyingPower));
+            output += fmt::format("\tEquityWithLoanValue:{}\n", std::to_string(equityWithLoanValue));
+            output += fmt::format("\tExcessLiquidity:{}\n", std::to_string(excessLiquidity));
+            output += fmt::format("\tFullAvailableFunds:{}\n", std::to_string(fullAvailableFunds));
+            output += fmt::format("\tFullExcessLiquidity:{}\n", std::to_string(fullExcessLiquidity));
+            output += fmt::format("\tFullInitMarginReq:{}\n", std::to_string(fullInitMarginReq));
+            output += fmt::format("\tFullMaintMarginReq:{}\n", std::to_string(fullMaintMarginReq));
+            output += fmt::format("\tGrossPositionValue:{}\n", std::to_string(grossPositionValue));
+            output += fmt::format("\tInitMarginReq:{}\n", std::to_string(initMarginReq));
+            output += fmt::format("\tLookAheadAvailableFunds:{}\n", std::to_string(lookAheadAvailableFunds));
+            output += fmt::format("\tLookAheadExcessLiquidity:{}\n", std::to_string(lookAheadExcessLiquidity));
+            output += fmt::format("\tLookAheadInitMarginReq:{}\n", std::to_string(lookAheadInitMarginReq));
+            output += fmt::format("\tLookAheadMaintMarginReq:{}\n", std::to_string(lookAheadMaintMarginReq));
+            output += fmt::format("\tMaintMarginReq:{}\n", std::to_string(maintMarginReq));
+            output += fmt::format("\tNetLiquidation:{}\n", std::to_string(netLiquidation));
+            output += fmt::format("\tTotalCashValue:{}\n", std::to_string(totalCashValue));
 
             return output;
         }
